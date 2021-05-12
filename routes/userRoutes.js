7c9486973c7b9152270bc0454af142f5d504cb32
Wunderay <60483832+wunderay@users.noreply.github.com>
2021-05-12 00:42:49 -0600

    Fixed logout issue

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,11 +6,11 @@ router.get("/new", usersController.new);
 router.post("/create", usersController.validate, usersController.create, usersController.redirectView);
 router.get("/login", usersController.login);
 router.post("/login", usersController.authenticate);
-router.post("/logout", usersController.logout, usersController.redirectView);
+router.get("/logout", usersController.logout, usersController.redirectView);
 
 router.get("/:id", usersController.show, usersController.showView);
 router.get("/:id/edit", usersController.edit);
-router.put("/:id/update", usersController.update, usersController.redirectView);
+router.put("/:id/update", usersController.validate, usersController.update, usersController.redirectView);
 router.delete("/:id/delete", usersController.delete, usersController.redirectView);
 
 module.exports = router;
