37d953fd3fcea7a6cb4ab4163919aca16515aa84
Wunderay <60483832+wunderay@users.noreply.github.com>
2021-05-11 23:35:27 -0600

    API Classwork

--- main.js
+++ main.js
@@ -1,108 +1,68 @@
-//const subscriber = require("./models/subscriber");
+"use strict";
 
-const express = require("express"), app = express(),
-router = express.Router(),
-homeController = require("./controllers/homeController"),
-errorController = require("./controllers/errorController"), 
-layouts = require("express-ejs-layouts"), 
-mongoose = require("mongoose"), 
-subscribersController = require("./controllers/subscribersController"),
-usersController = require("./controllers/usersController"),
-coursesController = require("./controllers/coursesController"),
-methodOverride = require("method-override"),
-cookieParser = require("cookie-parser"),
-expressSession = require("express-session"),
-expressValidator = require("express-validator"),
-passport = require("passport"),
-connectFlash = require("connect-flash")
-User = require("./models/user");
+const express = require("express"),
+    app = express(),
+    router = require("./routes/index"),
+    mongoose = require("mongoose"),
+    methodOverride = require("method-override"),
+    layouts = require("express-ejs-layouts"),
+    passport = require("passport"),
+    cookieParser = require("cookie-parser"),
+    expressSession = require("express-session"),
+    expressValidator = require("express-validator"),
+    connectFlash = require("connect-flash"),
+    User = require("./models/user");
 
 mongoose.connect(
-  "mongodb://localhost:27017/confetti_cuisine", 
-  {useNewUrlParser: true}
+    "mongodb://localhost:27017/confetti_cuisine", 
+    {useNewUrlParser: true}
 );
 mongoose.set("useCreateIndex", true);
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
 
-//middlewears
-router.use(methodOverride("_method", {methods: ["POST", "GET"]}));
-router.use(layouts);
-router.use(express.static("public"));
-router.use(expressValidator());
+app.use(methodOverride("_method", {methods: ["POST", "GET"]}));
+app.use(layouts);
+app.use(express.static("public"));
+app.use(expressValidator());
 
 app.use(
-  express.urlencoded({
-      extended: false
-  })
+    express.urlencoded({
+        extended: false
+    })
 );
 
-router.use(express.json());
+app.use(express.json());
 
 //sesion and cookies
-router.use(cookieParser("my_passcode"));
-router.use(expressSession({
-  secret: "my_passcode",
-  cookie: {
-      maxAge: 360000
-  },
-  resave: false,
-  saveUninitialized: false
+app.use(cookieParser("my_passcode"));
+app.use(expressSession({
+    secret: "my_passcode",
+    cookie: {
+        maxAge: 360000
+    },
+    resave: false,
+    saveUninitialized: false
 }));
 
-router.use(connectFlash());
+app.use(connectFlash());
 
-router.use(passport.initialize());
-router.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-router.use((req, res, next) => {
-  res.locals.flashMessages = req.flash();
-  res.locals.loggedIn = req.isAuthenticated();
-  res.locals.currentUser = req.user;
-  next();
+app.use((req, res, next) => {
+    res.locals.flashMessages = req.flash();
+    res.locals.loggedIn = req.isAuthenticated();
+    res.locals.currentUser = req.user;
+    next();
 })
-
-
-router.get("/", homeController.index);
-
-router.get("/users", usersController.index, usersController.indexView);
-router.get("/users/new", usersController.new);
-router.post("/users/create", usersController.validate, usersController.create, usersController.redirectView);
-router.get("/users/login", usersController.login);
-router.post("/users/login", usersController.authenticate);
-router.post("/users/logout", usersController.logout, usersController.redirectView);
-
-router.get("/users/:id", usersController.show, usersController.showView);
-router.get("/users/:id/edit", usersController.edit);
-router.put("/users/:id/update", usersController.update, usersController.redirectView);
-router.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
-
-router.get("/subscribers", subscribersController.index, subscribersController.indexView);
-router.get("/subscribers/new", subscribersController.new);
-router.post("/subscribers/create", subscribersController.create, subscribersController.redirectView);
-router.get("/subscribers/:id", subscribersController.show, subscribersController.showView);
-router.get("/subscribers/:id/edit", subscribersController.edit);
-router.put("/subscribers/:id/update", subscribersController.update, subscribersController.redirectView);
-router.delete("/subscribers/:id/delete", subscribersController.delete, subscribersController.redirectView);
-
-router.get("/courses", coursesController.index, coursesController.indexView);
-router.get("/courses/new", coursesController.new);
-router.post("/courses/create", coursesController.create, coursesController.redirectView);
-router.get("/courses/:id", coursesController.show, coursesController.showView);
-router.get("/courses/:id/edit", coursesController.edit);
-router.put("/courses/:id/update", coursesController.update, coursesController.redirectView);
-router.delete("/courses/:id/delete", coursesController.delete, coursesController.redirectView);
-
-router.use(errorController.pageNotFoundError );
-router.use(errorController.internalServerError );
 
 app.use("/", router);
 
 app.listen(app.get("port"), () => {
-    console.log(`Server is running on port: $ ${app.get("port")}`)
-
+    console.log(`Sever is running on port: ${app.get("port")}`);
 });
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const router = require("express").Router(),
+userRoutes = require("./userRoutes"),
+subscriberRoutes = require("./subscriberRoutes"),
+courseRoutes = require("./courseRoutes"),
+homeRoutes = require("./homeRoutes"),
+errorRoutes = require("./errorRoutes");
+
+router.use("/user", userRoutes);
+router.use("/courses", courseRoutes);
+router.use("/subscribers", subscriberRoutes);
+router.use("/", homeRoutes);
+router.use("/", errorRoutes);
+
+module.exports = router;
